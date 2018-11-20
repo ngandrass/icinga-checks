b945c939eb314ef29726df7ebb3d8cab7e984b6a
ngandrass <ngandrass@squacu.de>
2018-11-20 13:27:10 +0100

    check_moodle_login: Add support for new logintoken authentication

--- check_moodle_login.py
+++ check_moodle_login.py
@@ -9,42 +9,92 @@ __author__ = "Niels Gandraß"
 __email__ = "ngandrass@squacu.de"
 __copyright__ = "Copyright 2018, Niels Gandraß"
 __license__ = "MIT License"
-__version__ = "1.0.2"
+__version__ = "2.0.0"
 
 import sys
 import requests
 import argparse
+import re
+from enum import Enum
+
 from urllib.parse import urljoin
 
-# Parse arguments
-parser = argparse.ArgumentParser()
-parser.add_argument('-t', '--target', required=True, help='URL of the Moodle installation')
-parser.add_argument('-u', '--username', required=True, help='Username to use for authentication')
-parser.add_argument('-p', '--password', required=True, help='Password to use for authentication')
-args = parser.parse_args()
 
-# Try to login
-try:
-    login_request = requests.post(urljoin(args.target, 'login/index.php'), data={
-        'username': args.username,
-        'password': args.password
-    })
+class IcingaVerdict(Enum):
+    """
+    Exit codes for the corresponding verdicts
+    """
+    OK = 0,
+    WARNING = 1,
+    CRITICAL = 2,
+    UNKNOWN = 3
 
-    sc = login_request.status_code
 
-    if sc == 200 and login_request.url.endswith("/my/"):
-        print("OK - Login as", args.username, "successful")
-        sys.exit(0)
-    elif sc == 200 and not login_request.url.endswith("/my/"):
-        print("CRITICAL - Login as", args.username, "failed")
-        sys.exit(2)
-    elif (400 <= sc <= 405) or (500 <= sc <= 504):
-        print("CRITICAL - Failed with HTTP status code", sc)
-        sys.exit(2)
-    else:
-        print("UNKNOWN - Received HTTP status code: ", login_request.status_code)
-        sys.exit(3)
+def exit(verdict: IcingaVerdict, *msgs: str) -> None:
+    """
+    Prints test verdict and exits with appropriate exit code
+    :param verdict: Verdict of the test
+    :param msgs: Additional messages
+    :return: None
+    """
+    print(verdict.name, "-", " ".join(msgs))
+    sys.exit(verdict.value)
 
-except Exception as e:
-    print("UNKNOWN - Exception raised:", type(e).__name__)
-    sys.exit(3)
+
+def main():
+    # Parse arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-t', '--target', required=True, help='URL of the Moodle installation')
+    parser.add_argument('-u', '--username', required=True, help='Username to use for authentication')
+    parser.add_argument('-p', '--password', required=True, help='Password to use for authentication')
+    parser.add_argument('-s', '--skip-logintoken', required=False, help='Ignore moodle logintoken', action='store_true')
+    args = parser.parse_args()
+
+    # Initiate common session in order to supply a valid login token
+    s = requests.Session()
+
+    # Get login token
+    logintoken = ""
+    if not args.skip_logintoken:
+        try:
+            token_request = s.get(urljoin(args.target, 'login/index.php'))
+
+            if token_request.status_code == 200:
+                logintoken_input = re.search("<input type=\"hidden\" name=\"logintoken\" value=\"([a-zA-Z0-9]+)\">", token_request.text)
+
+                if not logintoken_input:
+                    exit(IcingaVerdict.UNKNOWN, "Couldn't find logintoken")
+                else:
+                    logintoken = logintoken_input.group(1)
+            else:
+                exit(IcingaVerdict.UNKNOWN, "Received HTTP status code during token request:", token_request.status_code)
+
+        except Exception as e:
+            exit(IcingaVerdict.UNKNOWN, "Exception raised during token request:", type(e).__name__)
+
+    # Try to login
+    try:
+        login_request = s.post(urljoin(args.target, 'login/index.php'), data={
+            'username': args.username,
+            'password': args.password,
+            'logintoken': logintoken
+        })
+
+        sc = login_request.status_code
+
+        # Evaluate login try
+        if sc == 200 and login_request.url.endswith("/my/"):
+            exit(IcingaVerdict.OK, "Login as", args.username, "successful")
+        elif sc == 200 and not login_request.url.endswith("/my/"):
+            exit(IcingaVerdict.CRITICAL, "Login as", args.username, "failed")
+        elif (400 <= sc <= 405) or (500 <= sc <= 504):
+            exit(IcingaVerdict.CRITICAL, "Failed with HTTP status code", sc)
+        else:
+            exit(IcingaVerdict.UNKNOWN, "Received HTTP status code:", sc)
+
+    except Exception as e:
+        exit(IcingaVerdict.UNKNOWN, "Exception raised during login:", type(e).__name__)
+
+
+if __name__ == "__main__":
+    main()
